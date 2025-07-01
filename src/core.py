7e324304374608d0f0821c26422549ef16737996
joovsns <trkulja.jovana@gimnazijaso.edu.rs>
2025-07-01 18:18:08 +0200

    Update
    Radi, ali bi se mogla korigovati velicina prozora. Takodje sadrzi nove TO-DO's

--- src/core.py
+++ src/core.py
@@ -3,11 +3,7 @@ from typing import Tuple
 import matplotlib.pyplot as plt
 import pygame
 
-pygame.init()
-
 SCREEN_WIDTH, SCREEN_HEIGHT = 120, 60
-screen = pygame.display.set_mode((SCREEN_WIDTH*10, SCREEN_HEIGHT*20))
-pygame.display.set_caption("slam brute force")
 
 senzor_radius = 10
 cilj = np.array([15, 70])
@@ -15,6 +11,12 @@ tmp_linspace = np.linspace(0, 2 * np.pi, 100)
 
 robot = {
     "pozicija": np.array([15, 15]),
+    "ugao": np.pi/4,
+    "robot_brzina": 1,
+}
+
+interni_robot = {
+    "pozicija": np.array([0, 0]),
     "ugao": np.pi/4,
     "robot_brzina": 1,
 }
@@ -35,16 +37,16 @@ mapa["celije"][20:30, 40:50] = 0
 
 def detekcija(senzor_radius: float, robot: dict, mapa: dict) -> np.ndarray:
     sken = np.zeros(mapa["celije"].shape)
-    for i in range(mapa["celije"].shape[0]):
+    for i in range(mapa["celije"].shape[0]): 
         for j in range(mapa["celije"].shape[1]):
             if (i - robot["pozicija"][0]) ** 2 + (j - robot["pozicija"][1]) ** 2 < senzor_radius ** 2:
                 sken[i][j] = mapa["celije"][i][j]
     return sken
 
-def mapiranje(interna_mapa: dict, sken: np.ndarray) -> np.ndarray:
+def mapiranje(interna_mapa: dict, sken: np.ndarray, robot: dict) -> np.ndarray:
     return 1 - (1 - interna_mapa["celije"]) * (1 - sken)
 
-def kretanje(robot: dict, pozicija: Tuple[float, float], cilj: np.ndarray, interna_mapa: dict) -> Tuple[float, float]:
+def kretanje(interni_robot: dict, pozicija: Tuple[float, float], cilj: np.ndarray, interna_mapa: dict) -> Tuple[float, float]:
     queue = [cilj.astype(int)]
     visited = np.zeros((interna_mapa["celije"].shape[0], interna_mapa["celije"].shape[1], 2))
     while len(queue) > 0:
@@ -60,7 +62,7 @@ def kretanje(robot: dict, pozicija: Tuple[float, float], cilj: np.ndarray, inter
     razlika = novi_cilj - robot["pozicija"]
     razlika = razlika / np.linalg.norm(razlika)
     pomeraj = razlika * robot["robot_brzina"]
-    return robot["pozicija"] + pomeraj
+    return robot["pozicija"] + pomeraj, interni_robot["pozicija"] + pomeraj
 
 
 def prikazi_skeniranje(screen, robot:dict, senzor_radius, tmp_linspace):
@@ -75,50 +77,77 @@ def prikazi_skeniranje(screen, robot:dict, senzor_radius, tmp_linspace):
                          (int(robot["pozicija"][1] * 10), int(robot["pozicija"][0] * 10)),
                          (int(x_end * 10), int(y_end * 10)), 1)
 
-running = True
-putanja = []
-putanja.append(robot["pozicija"])
-clock = pygame.time.Clock()
+def simulacija(robot: dict, pozicija: Tuple[float, float], cilj: np.ndarray, mapa: dict):
 
-# TODO : razdvojiti simulaciju od vizueliacije
+    for i in range(50):
+        sken = detekcija(senzor_radius, robot, mapa)
+        interna_mapa["celije"] = mapiranje(interna_mapa, sken)
+        istorija_int_mapa.append(interna_mapa["celije"])
+        robot["pozicija"] = kretanje(robot, robot["pozicija"], cilj, interna_mapa)
+        print(i, robot["pozicija"])
+        putanja.append(robot["pozicija"])
+
+    return putanja, istorija_int_mapa
+
+def vizuelizacija(mapa: dict, putanja: list, istorija_int_mapa: list):
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH*10, SCREEN_HEIGHT*20))
+    pygame.display.set_caption("slam brute force")
+
+    running = True
+    clock = pygame.time.Clock()
+        
+    while running:
+        #screen.fill((255, 255, 255))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        
+        # original mapa
+        for i in range(SCREEN_HEIGHT):
+            for j in range(SCREEN_WIDTH):
+                color = (0, 0, 0) if mapa["celije"][i, j] == 0 else (255, 255, 255)
+                pygame.draw.rect(screen, color, (j * 10, i * 10, 10, 10))
+
+        # interna mapa
+        for i in range(SCREEN_HEIGHT):
+            for j in range(SCREEN_WIDTH):
+                    color = (0, 0, 0) if interna_mapa["celije"][i, j] == 0 else (255, 255, 255)
+                    pygame.draw.rect(screen, color, (j * 10, i * 10 + SCREEN_HEIGHT * 10, 10, 10))
+
+        # robot i cilj
+        pygame.draw.circle(screen, (255, 0, 0), (int(robot["pozicija"][1] * 10), int(robot["pozicija"][0] * 10)), 5)
+        pygame.draw.circle(screen, (0, 255, 0), (int(cilj[1] * 10), int(cilj[0] * 10)), 5)
+
+        for p in putanja:
+            pygame.draw.circle(screen, (0, 0, 255), (int(p[1] * 10), int(p[0] * 10)), 2)
+    
+
+    # prikazi_matplotlib(robot, cilj, putanja, senzor_radius, tmp_linspace, mapa, interna_mapa)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+    pygame.quit()
+
+
+
+putanja = []
+istorija_int_mapa = []
+
+putanja, istorija_int_mapa = simulacija(robot, robot["pozicija"], cilj, mapa)
+
+#putanja.append(robot["pozicija"])
+
+vizuelizacija(mapa, putanja, istorija_int_mapa)
+
+# TODO : razdvojiti simulaciju od vizuelizacije
 # TODO : napraviti funkciju za simulaciju (pocetni polozaj robota, cilj, mapa) -> putanja, listu internih mapa
 # TODO : napraviti funkciju za vizuelizaciju (mapa, putanja, lista internih mapa)
 
-
-while running:
-    #screen.fill((255, 255, 255))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    sken = detekcija(senzor_radius, robot, mapa)
-    interna_mapa["celije"] = mapiranje(interna_mapa, sken)
-    robot["pozicija"] = kretanje(robot, robot["pozicija"], cilj, interna_mapa)
-    putanja.append(robot["pozicija"])
-
-    # obicna mapa
-    for i in range(SCREEN_HEIGHT):
-        for j in range(SCREEN_WIDTH):
-           color = (0, 0, 0) if mapa["celije"][i, j] == 0 else (255, 255, 255)
-           pygame.draw.rect(screen, color, (j * 10, i * 10, 10, 10))
-
-    # interna mapa
-    for i in range(SCREEN_HEIGHT):
-       for j in range(SCREEN_WIDTH):
-            color = (0, 0, 0) if interna_mapa["celije"][i, j] == 0 else (255, 255, 255)
-            pygame.draw.rect(screen, color, (j * 10, i * 10 + SCREEN_HEIGHT * 10, 10, 10))
-
-    # robot i cilj
-    pygame.draw.circle(screen, (255, 0, 0), (int(robot["pozicija"][1] * 10), int(robot["pozicija"][0] * 10)), 5)
-    pygame.draw.circle(screen, (0, 255, 0), (int(cilj[1] * 10), int(cilj[0] * 10)), 5)
-
-    for p in putanja:
-       pygame.draw.circle(screen, (0, 0, 255), (int(p[1] * 10), int(p[0] * 10)), 2)
-
-   # prikazi_matplotlib(robot, cilj, putanja, senzor_radius, tmp_linspace, mapa, interna_mapa)
-
-    pygame.display.flip()
-    clock.tick(30)
-
-pygame.quit()
+# TODO : dataset(sacuvane u fajlu) generisanih mapa, sa pocetnom i krajnjom tackom {pickle}
+# TODO : interna reprezentacija robota, sken treba da je matrica dimenzija [2*sken radius][2*sken radius], promeniti mapiranje da radi sa tom matricom skena, gde je siftujem za poziciju robota?
+# TODO : detekciju izmeniti, gledati da sken ne bude cela matrica vec samo deo koji je skeniran tj dimenzija [2*sken radius][2*sken radius], traziti odakle dokle ide for petlja itd
