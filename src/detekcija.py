7d1f0f744b9195f8460349f775ee637bbf5786ff
joovsns <trkulja.jovana@gimnazijaso.edu.rs>
2025-07-03 11:33:29 +0200

    Tren
    Izmenjeni sken sa dict koji radi

--- src/detekcija.py
+++ src/detekcija.py
@@ -9,6 +9,8 @@ def generisanjeUglova(n):
 
 def pucanjeLasera(x, y, mapa, n):
     udaljenostIUgao = []
+    sken_hit = []
+    sken_miss = []
 
     for ugao in generisanjeUglova(n):
         udaljenost = 0.0
@@ -21,16 +23,25 @@ def pucanjeLasera(x, y, mapa, n):
                 break
 
             if mapa[y0, x0] == 1: 
+                sken_hit.append((x0, y0))
                 break
-
+            else:
+                sken_miss.append((x0, y0))
+            
+            
             udaljenost = udaljenost + KORAK
+
+        #if udaljenost > RADIUS:  # Nema kontakta sa zidom
+         #   sken_miss.append((x0, y0))
 
         udaljenostIUgao.append((udaljenost, ugao))
 
-    return np.array(udaljenostIUgao)
+    return np.array(udaljenostIUgao), sken_hit, sken_miss
 
 def formiranjeSkena(x, y, mapa, n):
-    sken = np.zeros(mapa.shape)
+    sken_matrix = np.zeros(mapa.shape)
+    sken_hit = []
+    sken_miss = []
 
     for udaljenost, ugao in pucanjeLasera(x, y, mapa, n):
         if udaljenost > RADIUS: continue
@@ -40,29 +51,43 @@ def formiranjeSkena(x, y, mapa, n):
 
         if not (0 <= x0 < mapa.shape[1] and 0 <= y0 < mapa.shape[0]): break 
 
-        sken[y0, x0] = 1
+        sken_matrix[y0, x0] = 1
 
-    return sken
+    return {
+        "sken_matrix": sken_matrix,
+        "sken_hit": sken_hit,
+        "sken_miss": sken_miss
+    }
 
 def detekcija_relativna(senzor: dict, robot: dict, mapa1: dict) -> np.ndarray:
     
-    sken = formiranjeSkena(robot["pozicija"][0], robot["pozicija"][1], mapa1["celije"], n=360)  # n = broj uglova za laserski senzor
+    sken_data = formiranjeSkena(robot["pozicija"][0], robot["pozicija"][1], mapa1["celije"], n=360)  # n = broj uglova za laserski senzor
 
     centar_x, centar_y = int(robot["pozicija"][0]), int(robot["pozicija"][1])
 
     start_x = max(0, centar_x - 10)
     start_y = max(0, centar_y - 10)
-    end_x = min(sken.shape[1], centar_x + 10)
-    end_y = min(sken.shape[0], centar_y + 10)
+    end_x = min(sken_data["sken_matrix"].shape[1], centar_x + 10)
+    end_y = min(sken_data["sken_matrix"].shape[0], centar_y + 10)
 
-    sken_20x20 = sken[start_y:end_y, start_x:end_x]
+    sken_20x20 = sken_data["sken_matrix"][start_y:end_y, start_x:end_x]
 
-    return sken_20x20
+    return {
+        "sken": sken_20x20,
+        "hits": sken_data["sken_hit"],
+        "misses": sken_data["sken_miss"]
+    }
 
 def detekcija(senzor: dict, robot: dict, mapa1: dict) -> np.ndarray:
-    sken = np.zeros(mapa1["celije"].shape)
+    sken_data = {"sken_matrix": np.zeros(mapa1["celije"].shape), "sken_hit": [], "sken_miss": []}
+
     for i in range(mapa1["celije"].shape[0]): 
         for j in range(mapa1["celije"].shape[1]):
             if (i - robot["pozicija"][0]) ** 2 + (j - robot["pozicija"][1]) ** 2 < senzor["radius"] ** 2:
-                sken[i, j] = mapa1["celije"][i, j]
-    return sken
+                sken_data["sken_matrix"][i, j] = mapa1["celije"][i, j]
+                
+                if mapa1["celije"][i, j] == 1:
+                    sken_data["sken_hit"].append((i, j))
+                else:
+                    sken_data["sken_miss"].append((i, j))
+    return sken_data
--- src/mapiranje.py
+++ src/mapiranje.py
@@ -1,5 +1,6 @@
 from core import *
 
 
-def mapiranje(interna_mapa: dict, sken: np.ndarray, robot: dict) -> np.ndarray:
-    return 1 - (1 - interna_mapa["celije"]) * (1 - sken)
+def mapiranje(interna_mapa: dict, sken_data: dict, robot: dict) -> np.ndarray:
+    sken_matrix = sken_data["sken_matrix"]
+    return 1 - (1 - interna_mapa["celije"]) * (1 - sken_matrix)
